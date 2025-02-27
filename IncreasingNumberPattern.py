86ea5139b5be3a566c8259b6502d2809acf568da
sowmyadeva3 <113660263+sowmyadeva3@users.noreply.github.com>
2025-02-27 14:43:51 +0530

    Add files via upload

---
+++ IncreasingNumberPattern.py
@@ -0,0 +1,10 @@
+#Get n from user
+n = int(input())
+# Outer loop from 1 to N
+for i in range(1,n+1):
+    # inner loop from i to i
+    for j in range(1,i+1):
+        #print *
+        print("*",end=" ")
+    print()
+    
