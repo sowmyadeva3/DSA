233c3b4f1cabdf4cf16dc97e6a5d3938b358191e
sowmyadeva3 <113660263+sowmyadeva3@users.noreply.github.com>
2025-03-04 12:50:19 +0530

    Add files via upload

---
+++ OddSeriesSum.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def sumOfTheSeries (self, n):
+        sum = 0
+        for i in range(1,n+1):
+            sum = sum + int(math.pow(i,2))
+        return sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        
+        ob = Solution()
+        print(ob.sumOfTheSeries(n))
+# } Driver Code Ends
